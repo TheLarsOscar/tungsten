d537fce3bf7d10c771a5c79dea587fdfc94cf66a
TheLarsOscar <131546081+TheLarsOscar@users.noreply.github.com>
2025-07-05 18:24:35 +0200

    100 px orange

--- app/components/ContactBreakerOrange.tsx
+++ app/components/ContactBreakerOrange.tsx
@@ -2,7 +2,7 @@
 
 export default function ContactBreakerOrange() {
   return (
-    <section className="relative h-[220px] mb-breaker-gap flex items-center font-jetbrains uppercase text-left text-[28px] text-white pl-[16.6667%] overflow-hidden">
+    <section className="relative h-[100px] mb-breaker-gap flex items-center font-jetbrains uppercase text-left text-[28px] text-white pl-[16.6667%] overflow-hidden">
       <video
         className="absolute inset-0 w-full h-full object-cover z-0"
         autoPlay
